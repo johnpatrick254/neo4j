ac84a756bfa270213a2e59a19095812394ad2472
johnpatrick254 <jpattrick538@gmail.com>
2024-09-05 21:34:42 +0300

    update layout

--- src/components/ui/entitycard.tsx
+++ src/components/ui/entitycard.tsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 export const EntityCard = async ({ title, poster, rating = null, votes = null, runtime = null, genres = null, entity }) => {
     const baseURL = process.env.NEXT_PUBLIC_BASE_URL
 
-    return <Card className="h-48 w-40 shadow-md p-0.5 " >
+    return <Card className="h-52 w-44 shadow-md p-0.5 " >
         <Link href={`${baseURL}/${entity}/${title}`}>
             <CardContent className="p-1.5 h-3/4 relative text-xs">
                 <Image
@@ -13,17 +13,15 @@ export const EntityCard = async ({ title, poster, rating = null, votes = null, r
                     alt="entity-image"
                     height={50}
                     width={50}
-                    className="h-full w-full"
+                    className="h-full w-full transition-all ease-in-out duration-0 object-cover hover:scale-105 "
                 />
-                <span className="absolute top-2 right-3">{genres && genres.split('|')[0]}</span>
-                <span className="absolute bottom-2 right-3"> {runtime && runtime}</span>
             </CardContent>
             <CardFooter className="p-1.5">
                 <CardDescription className=" flex flex-col items-start gap-y-1 text-xs">
-                    <p className="truncate text-ellipsis w-40">{title}</p>
-                    <div className="flex justify-between">
-                        <p> {rating && `Rating: ${rating}`}</p>
-                        <p>{votes && votes}</p>
+                    <p className="truncate text-black font-semibold text-ellipsis w-40">{title}</p>
+                    <div className="flex gap-3 justify-between">
+                        <p> {rating && `Rating: ${Math.floor(rating)}`}</p>
+                        <p>{runtime && `Time:${(parseInt(runtime) * 10) + 'm'}`}</p>
                     </div>
 
                 </CardDescription>
@@ -31,3 +29,4 @@ export const EntityCard = async ({ title, poster, rating = null, votes = null, r
         </Link>
     </Card>
 }
+
